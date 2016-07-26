9ec1a57b0ed87cd2e4bdae41920107abe76040b5
unknown <davidgparker123@utexas.edu>
2016-07-26 15:43:07 -0500

    moved move code back

--- Assets/Scripts/TapToPlace.cs
+++ Assets/Scripts/TapToPlace.cs
@@ -3,20 +3,15 @@
 public class TapToPlace : MonoBehaviour
 {
     public bool Placing;
-    private Vector3 lastHandPos;
-    public float handSpeed = 1.0f;
 
     void Start()
     {
         Placing = false;
-        lastHandPos = new Vector3(0, 0, 0);
     }
 
     // Called by GazeGestureManager when the user performs a Select gesture
     void OnSelect()
     {
-        Debug.Log("TapToPlace - User clicked.");
-
         // On each Select gesture, toggle whether the user is in placing mode.
         Placing = !Placing;
 
@@ -43,31 +38,23 @@ public class TapToPlace : MonoBehaviour
 
         if (Placing)
         {
-            //// Do a raycast into the world that will only hit the Spatial Mapping mesh.
+            // Do a raycast into the world that will only hit the Spatial Mapping mesh.
             var headPosition = Camera.main.transform.position;
             var gazeDirection = Camera.main.transform.forward;
 
             RaycastHit hitInfo;
-
-            if (Physics.Raycast(headPosition, gazeDirection, out hitInfo, 30.0f, SpatialMapping.PhysicsRaycastMask))
+            if (Physics.Raycast(headPosition, gazeDirection, out hitInfo,
+                30.0f))
             {
+                // Move this object's parent object to
+                // where the raycast hit the Spatial Mapping mesh.
+                this.transform.position = hitInfo.point;
+
                 // Rotate this object's parent object to face the user.
                 Quaternion toQuat = Camera.main.transform.localRotation;
                 toQuat.x = 0;
                 toQuat.z = 0;
                 this.transform.rotation = toQuat;
-            }
-
-            if(Assets.Scripts.ReferenceStore.Instance.trackedHands.Count > 0)
-            {
-                var hand = Assets.Scripts.ReferenceStore.Instance.trackedHands[0];
-                Vector3 newPos = new Vector3(0, 0, 0);
-
-                if(hand.properties.location.TryGetPosition(out newPos))
-                {
-                    this.transform.position += (lastHandPos - newPos) * handSpeed;
-                    lastHandPos = newPos;
-                }
             }
         }
     }
