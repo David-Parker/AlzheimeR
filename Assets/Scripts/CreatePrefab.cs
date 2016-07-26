db33eceacb875428b897eb955222bbc852677bd0
unknown <davidgparker123@utexas.edu>
2016-07-25 20:53:11 -0500

    Starting object dupe

---
+++ Assets/Scripts/CreatePrefab.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatePrefab : MonoBehaviour {
+
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnSelect()
+    {
+        Instantiate(prefab, new Vector3(3, 0, 3), Quaternion.identity);
+    }
+}
